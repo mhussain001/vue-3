625f132799116df493bb3db2b8d164f36648de89
Mohammad Hussain <mohammad.hussain@nagarro.com>
2021-08-25 10:28:07 +0530

    Added topbar component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <TopBar />
   <router-view />
 </template>
+
+<script>
+import TopBar from "./components/TopBar.vue";
+
+export default {
+  components: [TopBar],
+  setup() {},
+};
+</script>
 
 <style lang="scss">
 #app {
